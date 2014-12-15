c23bf14de3bd9fa3d0e58e5545d4fe9c19d8e428
Martin Warin <mwarin@umich.edu>
2014-12-15 13:59:51 -0500

    new files

---
+++ lib/htph/hathibench.rb
@@ -0,0 +1,68 @@
+# A simple time benchmarking tool, intended to be used like so:
+
+# require 'htph';
+# b = HTPH::Hathibench::Benchmark.new();
+# b.time("foo") do
+#   some_lengthy_method();
+# end
+# puts b.prettyprint(); # Reports max, min, total, avg, etc.
+
+# ... for benchmarking an arbitrary number of arbitrary code blocks.
+# b.time() either takes a label, or if left blank, uses the caller
+# path and line as label.
+
+# Any number of labels can be timed, and each label can be used to
+# time different things, if so wished.
+
+module HTPH::Hathibench
+  class Benchmark
+    def initialize ()
+      # Hash of Profile objects, keyed on their label.
+      @profiles = {};
+    end
+    def time (label = caller(1)[0].split(':')[0,2].join(':')) 
+      # ^^ If no label given, use caller path + lineno. ^^
+      @profiles[label] ||= Profile.new(label);
+      ts = Time.new();
+      yield;
+      te = Time.new();
+      # get duration as epoch-in-seconds.milliseconds
+      duration = te.strftime("%s.%L").to_f - ts.strftime("%s.%L").to_f;
+      @profiles[label].update(duration);
+    end    
+    def prettyprint
+      self.to_s; # Just an alias.
+    end
+    def to_s
+      out = [%w[label time calls max min avg].join("\t")];
+      @profiles.keys.sort.each do |label|
+        out << @profiles[label].to_s;
+      end
+      return out.join("\n");
+    end
+  end
+
+  # Benchmarking profile for a particular label.
+  class Profile    
+    def initialize (label)
+      @label = label;
+      @time  = 0.0;
+      @calls = 0;
+      @max   = 0.0;
+      @min   = Float::MAX;
+    end
+    def update (duration)
+      @time  += duration;
+      @calls += 1;
+      @max    = duration if duration > @max;
+      @min    = duration if duration < @min;
+    end
+    def to_s
+      avg = 0;
+      if @calls > 0 then 
+        avg = @time / @calls;
+      end
+      return [@label, @time, @calls, @max, @min, avg].join("\t");
+    end
+  end
+end
---
+++ lib/htph/hathinormalize.rb
@@ -0,0 +1,61 @@
+module HTPH::Hathinormalize
+  def self.agency (ag)
+    ag.upcase!;
+    ag.gsub!(/[,\.:;]|\'S?/, '');   # punctuations
+    ag.gsub!(/[\(\)\{\}\[\]]/, ''); # Brackets
+    ag.gsub!(/FOR SALE BY.*/, '');  # I AM NOT INTERESTED IN WHAT YOU ARE SELLING KTHXBYE.
+    ag.gsub!(/PRINTED (AT|BY).*/, '');   # Don't care where you got prunt or who prant you.
+    ag.gsub!(/DISTRIBUTED BY.*/, '');                  # Shoot the messenger.
+    ag.gsub!(/AVAILABLE (BY|FOR|FROM|THROUGH|TO).*/, ''); # Don't care.
+    ag.gsub!(/.*COPIES.*/, ''); # Don't care.
+    ag.gsub!(/^PREPARED .*/, ''); # Don't care.
+    ag.gsub!(/SUPT OF.*/, '');
+    ag.gsub!(/\b(THE) /, ''); # Stop words
+    # Abbreviations et cetera.
+    ag.gsub!(/DEPARTMENT/, 'DEPT');
+    ag.gsub!(/DEPTOF/, 'DEPT OF'); # Strangely common typo(?)
+    ag.gsub!(/&/, ' AND ');
+    ag.gsub!(/UNITED STATES( OF AMERICA)?/, 'US');
+    ag.gsub!(/U\sS\s|U S$/, 'US ');
+    ag.gsub!(/GOV(ERNMEN|\')T/, 'GOVT');
+    ag.gsub!(/ (SPN|RES)$/, '');
+    # US GOVT PRINT OFF, which is so common yet has so many variations.
+    ag.sub!(/(US\s?)?GOVT\s?PRINT(ING)?\s?OFF(ICE)?/, 'USGPO');
+    ag.sub!(/U\s?S\s?G\s?P\s?O/, 'USGPO');
+    ag.sub!(/^GPO$/, 'USGPO');
+    ag.sub!(/AUTHOR *$/, '');
+    ag.sub!(/\?/, ''); # Be sure.
+    ag.gsub!(/ +/, ' '); # whitespace
+    ag.sub!(/^ +/,  '');
+    ag.sub!(/ +$/,  '');
+    return ag;
+  end
+
+  def self.enumc (e)
+    e.upcase!;
+    e.gsub!(/ +/, " ");
+
+    # Deal with copies
+    e.gsub!(/(C|COPY?)[ .]*\d+/, "");
+    e.gsub!(/(\d+(ST|ND|RD|TH)|ANOTHER) COPY/, "");
+
+    e.gsub!(/VOL(UME)?/, "V");
+    e.gsub!(/[\(\)\[\]\*]/, "");
+    e.gsub!(/SUPP?(L(EMENT)?)?S?/, "SUP");
+    e.gsub!(/&/, " AND ");
+    e.gsub!(/\.(\S)/, ". \\1");
+    e.gsub!(/\*/, "");
+    # e.gsub!(/ \-/, "-");
+    e.strip!;
+    
+    return e;
+  end
+
+  def self.title (t)
+    t.gsub!(/[^A-Z0-9]+/, ' ');      
+    t.gsub!(/ +/, ' '); # whitespace 
+    t.sub!(/^ /,  '');               
+    t.sub!(/ $/,  '');               
+    return t;                        
+  end
+end
